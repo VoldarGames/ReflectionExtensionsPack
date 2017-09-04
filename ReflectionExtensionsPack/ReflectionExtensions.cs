af729ee3d879ffaeba1c9f57f553d24ac149ae93
DaniGarciaSanchez <dgs88dgs88@gmail.com>
2017-09-04 21:16:27 +0200

    Added Implements method and test

---
+++ ReflectionExtensionsPack/ReflectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Reflection;
+
+namespace ReflectionExtensionsPack
+{
+    public static class ReflectionExtensions
+    {
+        /// <summary>
+        /// Returns true if type implements an interface T,
+        ///  returns false if not or T is not an interface or is self
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        public static bool Implements<T>(this Type type)
+        {
+            return typeof(T) != type && typeof(T).GetTypeInfo().IsInterface && typeof(T).GetTypeInfo().IsAssignableFrom(type.GetTypeInfo());
+        }
+    }
+}
---
+++ ReflectionExtensionsTest/UnitTest1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ReflectionExtensionsPack;
+
+namespace ReflectionExtensionsTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ImplementsMethodOkTest()
+        {
+            Assert.IsTrue(typeof(Dog).Implements<IKillable>());
+            Assert.IsFalse(typeof(IKillable).Implements<IKillable>());
+            Assert.IsFalse(typeof(Dog).Implements<Dog>());
+            Assert.IsFalse(typeof(Dog).Implements<Animal>());
+            Assert.IsFalse(typeof(Dog).Implements<int>());
+
+        }
+    }
+
+    public class Dog : Animal
+    {
+        
+    }
+
+    public class Animal : IKillable
+    {
+        public void Die()
+        {
+            Console.WriteLine("I'm dead :(");
+        }
+    }
+
+    public interface IKillable
+    {
+        void Die();
+    }
+}
